434249726b2372eb8562ff7427783acc1a708f86
sam23 <sam.2.3.4.5.ranjan@gmail.com>
2023-03-17 01:41:41 +0530

    Guide to Jetpack Compose Effect Handlers Part-10

--- app/src/main/java/com/realm/tutorial/MainActivity.kt
+++ app/src/main/java/com/realm/tutorial/MainActivity.kt
@@ -4,6 +4,8 @@ import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -39,6 +41,7 @@ import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
 import com.realm.tutorial.ui.theme.TutorialTheme
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlin.random.Random
 
@@ -60,205 +63,47 @@ class MainActivity : ComponentActivity() {
     )
 
     setContent {
-      val constraints = ConstraintSet{
-        val greenBox = createRefFor("greenbox")
-        val redBox = createRefFor("redbox")
-        val guideLine = createGuidelineFromTop(0.5f)
-
-        constrain(greenBox) {
-          top.linkTo(guideLine)
-          start.linkTo(parent.start)
-          width = Dimension.value(100.dp)
-          height = Dimension.value(100.dp)
-        }
-        constrain(redBox) {
-          top.linkTo(parent.top)
-          start.linkTo(greenBox.end)
-          width = Dimension.value(100.dp)
-          height = Dimension.value(100.dp)
-        }
-        createHorizontalChain(greenBox, redBox, chainStyle = ChainStyle.Packed)
+      var sizeState by remember {
+        mutableStateOf(200.dp)
       }
+      val size by animateDpAsState(
+        targetValue = sizeState,
+        keyframes {
+          durationMillis = 5000
+          sizeState at 0 with LinearEasing
+          sizeState * 1.5f at 1000 with FastOutLinearInEasing
+          sizeState * 2f at 5000
+        }
+//        spring(
+//          dampingRatio = Spring.DampingRatioHighBouncy
+//        )
+//        tween(
+//          durationMillis = 3000,
+//          delayMillis = 300,
+//          easing = LinearOutSlowInEasing
+//        )
+      )
 
-      ConstraintLayout(constraints, modifier = Modifier.fillMaxSize()) {
-        Box(modifier = Modifier
-          .background(Color.Green)
-          .layoutId("greenbox")
+      val infiniteTransition = rememberInfiniteTransition ()
+      val color by infiniteTransition.animateColor(
+        initialValue = Color.Red,
+        targetValue = Color.Green,
+        animationSpec = infiniteRepeatable(
+          tween(durationMillis = 2000),
+          repeatMode = RepeatMode.Reverse
         )
-        Box(modifier = Modifier
-          .background(Color.Red)
-          .layoutId("redbox")
-        )
+      )
+
+
+      Box(modifier = Modifier
+        .size(size)
+        .background(color),
+        contentAlignment = Alignment.Center
+      ) {
+        Button(onClick = { sizeState += 50.dp }) {
+          Text(text = "Increase Size")
+        }
       }
-//      val painter = painterResource(id = R.drawable.female)
-//      val description = "A female model wearing nice goggle"
-//      val title = "Devente Ebison Collections"
-//      val color = remember {
-//        mutableStateOf(Color.Yellow)
-//      }
-//      val scaffoldState = rememberScaffoldState()
-//      var textFieldState by remember {
-//        mutableStateOf("")
-//      }
-//      val scope = rememberCoroutineScope()
-//
-//      Column(
-//        Modifier.fillMaxSize()
-//      ) {
-//        Box(modifier = Modifier
-//          .fillMaxWidth(0.5f)
-//          .padding(12.dp)
-//        ) {
-//         ImageCard(painter = painter, contentDescription = description, title = title)
-//       }
-//        Box(modifier = Modifier
-//          .fillMaxWidth()
-//          .background(Color(0xFF101010))
-//        ) {
-//         Text(
-//           text = buildAnnotatedString {
-//              withStyle(
-//                style = SpanStyle(
-//                  color = Color.Green,
-//                  fontSize = 50.sp
-//                )
-//              ) {
-//                append("J")
-//              }
-//             append("etpack ")
-//             withStyle(
-//               style = SpanStyle(
-//                 color = Color.Green,
-//                 fontSize = 50.sp
-//               )
-//             ) {
-//               append("C")
-//             }
-//             append("ompose")
-//           },
-//           color= Color.White,
-//           fontSize = 30.sp,
-//           fontFamily = fontFamily,
-//           fontWeight = FontWeight.Bold,
-//           fontStyle = FontStyle.Italic,
-//           textAlign = TextAlign.Center,
-//           textDecoration = TextDecoration.Underline
-//         )
-//       }
-//        ColorBox(modifier = Modifier.weight(1f).fillMaxSize()) {
-//          color.value = it
-//        }
-//        Box(modifier = Modifier.background(color.value).weight(1f).fillMaxSize())
-//        Scaffold(modifier = Modifier
-//          .fillMaxWidth()
-//          .background(Color.Magenta), scaffoldState = scaffoldState) {it
-//          Column(
-//            horizontalAlignment = Alignment.CenterHorizontally,
-//            verticalArrangement = Arrangement.Center,
-//            modifier = Modifier
-//              .padding(horizontal = 30.dp)
-//          ) {
-//              TextField(
-//                value = textFieldState,
-//                label = {
-//                  Text(text = "Enter your name")
-//                },
-//                onValueChange = {
-//                  textFieldState = it
-//                },
-//                singleLine = true,
-//                modifier = Modifier.fillMaxWidth()
-//              )
-//              Spacer(modifier = Modifier.height(16.dp))
-//              Button(onClick = {
-//                scope.launch {
-//                  scaffoldState.snackbarHostState.showSnackbar("Hello $textFieldState")
-//                }
-//              }) {
-//                Text(text = "Please greet me")
-//              }
-//          }
-//        }
-//        LazyColumn(
-//          modifier = Modifier
-//        ) {
-//          itemsIndexed(
-//            listOf("This", "is", "Jetpack", "Compose")
-//          ) {index, string ->
-//            Text(
-//              text = string,
-//              modifier = Modifier
-//                .fillMaxWidth()
-//                .padding(vertical = 24.dp),
-//              fontSize = 24.sp,
-//              fontWeight = FontWeight.Bold,
-//              textAlign = TextAlign.Center,
-//            )
-//          }
-//        }
-//      }
-//    }
-//  }
-}}}
-
-//@Composable
-//fun ImageCard(
-//  painter: Painter,
-//  contentDescription: String,
-//  title: String,
-//  modifier: Modifier = Modifier
-//){
-//  Card(
-//    modifier = modifier.fillMaxWidth(),
-//    shape = RoundedCornerShape(15.dp),
-//    elevation = 5.dp
-//  ) {
-//    Box(modifier = Modifier.height(200.dp)) {
-//      Image(
-//        painter = painter,
-//        contentDescription = contentDescription,
-//        contentScale = ContentScale.Crop
-//      )
-//      Box(modifier= Modifier
-//        .fillMaxSize()
-//        .background(
-//          brush = Brush.verticalGradient(
-//            colors = listOf(
-//              Color.Transparent,
-//              Color.Black
-//            ),
-//            startY = 300f
-//          )
-//        )
-//      ){}
-//      Box(
-//        modifier = Modifier
-//          .fillMaxSize()
-//          .padding(12.dp),
-//        contentAlignment = Alignment.BottomStart
-//      ) {
-//        Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
-//      }
-//    }
-//  }
-//}
-
-//@Composable
-//fun ColorBox(
-//  modifier: Modifier = Modifier,
-//  updateColor: (Color) -> Unit
-//) {
-//  Box(modifier = modifier
-//    .background(Color.Red)
-//    .clickable {
-//      updateColor(
-//        Color(
-//          Random.nextFloat(),
-//          Random.nextFloat(),
-//          Random.nextFloat(),
-//        )
-//      )
-//    }
-//  )
-//}}}
-
+    }
+  }
+}
